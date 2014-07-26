37f9fe2a351058cecb066bd48aeecee1f336b396
Ming-Shing Chen <mschen@crypto.tw>
2014-07-26 16:50:31 +0800

    suggested dh interface.

--- include/polarssl/dh.h
+++ include/polarssl/dh.h
@@ -36,4 +36,44 @@ typedef struct {
     void *dh_ctx;
 } dh_context_t;
 
+
+typedef struct {
+    dh_type_t type;
+    const char *name;
+
+    void * (*ctx_alloc)( void );
+    void (*ctx_free)( void *ctx );
+
+    /* computation */
+    int (*gen_public)( void *ctx, int (*f_rng)(void *, unsigned char *, size_t), void *p_rng );
+    int (*compute_shared)( void *ctx , int (*f_rng)(void *, unsigned char *, size_t),
+                         void *p_rng );
+
+    /* IO */
+    /* setting */
+    int (*set_params)( void *ctx , int params );
+    int (*read_params)( void *ctx , const unsigned char *buf , size_t blen );
+    int (*read_params_from_pk_ctx)( void *ctx , const void *pk_ctx);
+
+    size_t (*get_size_params)( const void *ctx );
+    int (*write_params)( size_t * olen , unsigned char *buf, size_t blen, const void *ctx );
+
+    /* read peer's public from buf */
+    int (*read_public) ( void *ctx, const unsigned char *buf, size_t blen );
+    int (*read_public_from_pk_ctx)( void *ctx, const void *pk_ctx);
+
+    /* write our's public to buf */
+    size_t (*get_size_pk)( const void *ctx );
+    int (*write_public)( size_t * olen , unsigned char *buf, size_t blen, const void *ctx );
+
+    int (*write_pre_master) ( size_t * olen, unsigned char *buf, size_t blen, const void *ctx );
+
+} dh_info2_t;
+
+typedef struct {
+    const dh_info2_t *dh_info;
+    void *dh_ctx;
+} dh_context2_t;
+
+
 #endif
