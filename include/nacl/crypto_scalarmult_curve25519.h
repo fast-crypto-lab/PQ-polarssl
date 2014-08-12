e7239380ad4d6a0922da579c79e4a16e32d43ab6
Ming-Shing Chen <mschen@crypto.tw>
2014-08-13 00:21:36 +0800

    add nacl/curve25519.h

---
+++ include/nacl/crypto_scalarmult_curve25519.h
@@ -0,0 +1,12 @@
+#ifndef crypto_scalarmult_curve25519_H
+#define crypto_scalarmult_curve25519_H
+
+
+extern int crypto_scalarmult_curve25519(unsigned char *,const unsigned char *,const unsigned char *);
+extern int crypto_scalarmult_curve25519_base(unsigned char *,const unsigned char *);
+
+#define crypto_scalarmult_curve25519_BYTES 32
+#define crypto_scalarmult_curve25519_SCALARBYTES 32
+
+
+#endif
